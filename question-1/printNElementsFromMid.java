2ca592568030e3ae050379a9eecbdaac5fabeab3
ritikajagtap <ritika.jagtap.2002@gmail.com>
2024-05-11 12:38:26 +0530

    printing n elements from the middle of the string

---
+++ question-1/printNElementsFromMid.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+
+public class printNElementsFromMid{
+    public static void main(String[] args){
+      Scanner scanner = new Scanner(System.in);
+      System.out.println("Enter the string: ");
+      String str = scanner.nextLine();
+      int n  = scanner.nextInt();
+      int mid = str.length()/2;
+      //System.out.println("mid of string is : " + mid + " " + str.charAt(mid));
+      int ind = Math.min(mid+n, str.length());
+      System.out.println(str.substring(mid, ind));
+    }
+}

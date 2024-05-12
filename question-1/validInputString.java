0a7f48feff3d1be63c80615d06bc872b3e84fd2c
ritikajagtap <ritika.jagtap.2002@gmail.com>
2024-05-12 11:29:33 +0530

    Is the string valid or not added

---
+++ question-1/validInputString.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class validInputString{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        String s1 = scanner.nextLine();
+        String s2 = scanner.nextLine();
+        Solution sol = new Solution();
+        if(sol.f(s1, s2)){
+            System.out.println("Input is valid.");
+        }else{
+            System.out.println("Input is invalid.");
+
+        }
+    }
+}
+
+class Solution{
+    public boolean f(String s1, String s2){
+        HashMap<Character, Integer> mp = new HashMap<>();
+        for(int i=0; i<s2.length(); i++){
+            char tmp = s2.charAt(i);
+            mp.put(tmp, mp.getOrDefault(tmp, 0)+1);
+        }
+        for(int i=0; i<s1.length(); i++){
+            char c = s1.charAt(i);
+            if(!mp.containsKey(c)){return false;}
+        }
+        return true;
+    }
+}
